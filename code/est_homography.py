2cbb801b0674a4c6fba08a35537d1f61f437a253
Jayadev1402 <89912646+Jayadev1402@users.noreply.github.com>
2023-01-27 09:58:37 -0500

    Update est_homography.py

--- code/est_homography.py
+++ code/est_homography.py
@@ -1,19 +1,7 @@
 import numpy as np
 
 def est_homography(X, Y):
-    """ 
-    Calculates the homography H of two planes such that Y ~ H*X
-    If you want to use this function for hw5, you need to figure out 
-    what X and Y should be. 
-    Input:
-        X: 4x2 matrix of (x,y) coordinates 
-        Y: 4x2 matrix of (x,y) coordinates
-    Returns:
-        H: 3x3 homogeneours transformation matrix s.t. Y ~ H*X
-        
-    """
-    
-    ##### STUDENT CODE START #####
+
     X_ = np.hstack([X, np.ones([X.shape[0],1])])
     
     A = np.zeros([8,9])
@@ -26,6 +14,5 @@ def est_homography(X, Y):
     H = V[-1,:] / V[-1, -1]
     H = np.reshape(H, [3,3])
     
-    ##### STUDENT CODE END #####
     
     return H
