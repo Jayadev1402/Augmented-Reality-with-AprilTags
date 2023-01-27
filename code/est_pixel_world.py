e538cda5adb2a4749ca3a3905da118ef03d6b738
Jayadev1402 <89912646+Jayadev1402@users.noreply.github.com>
2023-01-27 09:58:53 -0500

    Update est_pixel_world.py

--- code/est_pixel_world.py
+++ code/est_pixel_world.py
@@ -1,19 +1,7 @@
 import numpy as np
 
 def est_pixel_world(pixels, R_wc, t_wc, K):
-    """
-    Estimate the world coordinates of a point given a set of pixel coordinates.
-    The points are assumed to lie on the x-y plane in the world.
-    Input:
-        pixels: N x 2 coordiantes of pixels
-        R_wc: (3, 3) Rotation of camera in world
-        t_wc: (3, ) translation from world to camera
-        K: 3 x 3 camara intrinsics
-    Returns:
-        Pw: N x 3 points, the world coordinates of pixels
-    """
 
-    ##### STUDENT CODE START #####
     R_wc=R_wc.T
     t_wc1 = -R_wc@t_wc
 
@@ -30,6 +18,5 @@ def est_pixel_world(pixels, R_wc, t_wc, K):
     new=new/new[:,-1].reshape(-1,1)
     new[:,-1]=0
 
-    # return np.inv(H) @ np.inv(K)  @ p
-    ##### STUDENT CODE END #####
+
     return new
